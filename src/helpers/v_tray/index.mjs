43b42cbd478ea8c879516e5f0bfab5aabd3950f9
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2021-09-27 19:37:43 +0200

    yea

---
+++ src/helpers/v_tray/index.mjs
@@ -0,0 +1,53 @@
+
+
+import SysTray from 'systray';
+
+const Vtray =()=> {
+  let systray = new SysTray.default({
+    menu: {
+      icon: "<base64 image string>",
+      title: "标题",
+      tooltip: "Tips",
+      items: [{
+        title: "FirstSampleTitle",
+        tooltip: "FirstTitleToolTip",
+        checked: true, // checked not implemented on Linux yet.
+        enabled: true,
+        trigger(action) {
+          console.log("title: FirstSampleTitle");
+          systray.sendAction({ type: 'update-item', item: { ...action.item, checked: !action.item.checked, }, seq_id: action.seq_id, });
+          return "FirstSampleTitle";
+        },
+      }, {
+        title: "aa2",
+        tooltip: "bb",
+        checked: false,
+        enabled: true,
+        trigger(action) {
+          console.log("title: aa2");
+          return "aa2";
+        },
+      }, {
+        title: "Exit",
+        tooltip: "bb",
+        checked: false,
+        enabled: true,
+        trigger(action) {
+          console.log("EXITING...");
+          systray.kill();
+          return "Exit";
+        },
+      }]
+    },
+    debug: false,
+    copyDir: true, // copy go tray binary to outside directory, useful for packing tool like pkg.
+  });
+
+  systray.onClick(action => {
+    systray._conf.menu.items[action.seq_id].trigger(action);
+  })
+}
+
+Vtray();
+
+module.exports = Vtray;
