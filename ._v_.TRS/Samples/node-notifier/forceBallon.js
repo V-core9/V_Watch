b8f3623051f89bdbc1ffeb5f090148f753fe8cd6
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-03-30 16:36:50 +0200

    yea super cleanup v1 test

--- ._v_.TRS/Samples/node-notifier/forceBallon.js
+++
@@ -1,9 +0,0 @@
-const notifier = require('node-notifier');
-const balloon = notifier.WindowsBalloon();
-balloon
-  .notify({ message: 'Hello' }, function(err, data) {
-    console.log(err, data);
-  })
-  .on('click', function() {
-    console.log(arguments);
-  });
