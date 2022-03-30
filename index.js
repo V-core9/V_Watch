36e5459b57408cd2e5b8620823194dce4ef74c1f
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-03-30 16:35:38 +0200

    yea

--- index.js
+++ index.js
@@ -18,13 +18,19 @@ const v_tray = require('./src/helpers/v_tray');
 const V_Watch = require('./src/v_watch');
 const vWatch = new V_Watch({ interval: 50 });
 
+
 //* DEMO/SAMPLE TASKS TO RUN
 vWatch.newTask("printToConsole", 500, () => console.log("printToConsole PRINT TO CONSOLE TASK"));
 vWatch.newTask("justDoIt", 750, () => console.log("justDoIt PRINT TO CONSOLE TASK"));
 
+
 //! FEW REAL TASKS 
+
+// This will do the rendering of vBackgroundGUI.
 vWatch.newTask("vBackgroundGUI", 2000, async () => await vBackgroundGUI.render());
+// Setting Task status to match settings.
 vWatch.setTaskStatus("vBackgroundGUI", config.backgroundUpdates);
+
 
 
 // Exit Handler
