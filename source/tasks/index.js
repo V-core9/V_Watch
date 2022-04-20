b09ae8560632e374349f6a179316137314c98ccf
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-04-20 10:54:58 +0200

    yea

--- source/tasks/index.js
+++ source/tasks/index.js
@@ -16,10 +16,12 @@ const systemInfoStats = require('./system-info-stats');
 //? Clock Update
 const clockUpdate = require('./clock-update');
 
+//? NPM Total Downloads for list of packages
+const totalDownloads = require('./total-downloads');
+
 //? Debugging
 const vWatchDebug = require('./vWatchDebug');
 
-const totalDownloads = require('./total-downloads');
 
 
 const config = require('../config');
@@ -36,33 +38,24 @@ module.exports = sysTasks = async () => {
   await vWatch.setTaskStatus("wallpaperGUI", config.backgroundUpdates);
 
 
-
   //* DEMO/SAMPLE TASKS TO have [DISABLED]
   await vWatch.newTask("justDoIt", 750, justDoIt, "Demo Task Description Placeholder");
   await vWatch.disableTask("justDoIt");
 
 
-
   //* CLOCK Task
   await vWatch.newTask("clock", vTime.seconds(config.redrawTime), clockUpdate, "vWatch task that updates Clock in cache");
-
-
 
   //* Getting Current User&Device Info
   await vWatch.newTask("systemInfoStats", vTime.seconds(config.redrawTime), async () => await systemInfoStats(vTime.seconds(config.redrawTime)), "Getting Current User and System Info");
 
-
-
   //* Internet Speed Test
   await vWatch.newTask("netSpeedTest", vTime.minutes(30), netSpeedTest, "Internet Speed Test");
-  //await vWatch.disableTask("netSpeedTest");
 
-
+  //* NPM Downloads
+  await vWatch.newTask("totalDownloads", vTime.days(1), totalDownloads, "Total Downloads");
 
   //* vWatch Info Cache
   await vWatch.newTask("vWatchDBG", vTime.minutes(1), vWatchDebug, "vWatch Info Cache");
-
-
-  await vWatch.newTask("totalDownloads", vTime.days(1), totalDownloads, "Total Downloads");
 
 };
