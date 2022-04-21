954c2ff252333f25ef4de2ee8a922d7dc33647fd
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-04-21 19:11:35 +0200

    fix to autostart

--- source/core/index.js
+++ source/core/index.js
@@ -2,6 +2,6 @@ const V_Core_Cache = require('v_core_cache');
 const V_Watch  = require('./v_watch');
 
 module.exports = {
-  vWatch: new V_Watch({ interval: 500 }),
+  vWatch: new V_Watch({ autoStart: true, interval: 250 }),
   cache: new V_Core_Cache()
 };
