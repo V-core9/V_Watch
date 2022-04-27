c7fa241e7eb07b8be2ba1fb0e84e6c487375e33e
⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-04-27 12:30:04 +0200

    cleanup

--- source/core/index.js
+++ source/core/index.js
@@ -1,8 +1,6 @@
 const Watch = require('./Watch');
-const Wallpaper = require('./Wallpaper');
 
 module.exports = {
   cache: require('./cache'),
   watch: new Watch(),
-  wallpaper: new Wallpaper({ quality: 25 })
 };
